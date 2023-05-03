91e5fdf74ee062dc82dd2329d0469c3c0eb0e5ba
Gabriel Acassio <100379848+GacAcassio@users.noreply.github.com>
2023-05-03 08:24:23 -0400

    Create code.cpp

---
+++ 1/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int indice = 13, soma = 0, k = 0;
+  
+  while(k < indice)
+  {
+    k++;
+    soma += k;
+  }
+  
+  cout << soma;
+  
+  return 0;
+}
